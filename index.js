e750a28d08728365cb68cc7d8bb79be8727f0ba6
J Scott Cromie <scromie@deloitte.com>
2017-07-25 18:20:42 +0000

    adding SFDC connection

--- index.js
+++ index.js
@@ -65,3 +65,28 @@ app.get('/bins', function (req, res, next) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else 
+       { response.render('pages/db', {results: result.rows} ); }
+    }
+  }
+});
+
+app.get('/salesforce', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM salesforce.contact', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    }
+  });
+}
